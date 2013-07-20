a6431609b91e00ac141f20ac994b52c0b060d59f
btag <jhonathan.facin@gmail.com>
2013-07-20 02:53:06 -0300

    Update opshue.py

--- opshue.py
+++ opshue.py
@@ -9,24 +9,21 @@ import re
 import unicodedata as un
 import webbrowser as wb
 
-def noaccent(string):
+def noaccent(string): # No caso de alguém querer traduzir o assistente para o português, é melhor remover os acentos das palavras-chave
   return un.normalize('NFKD', string.decode('utf-8')).encode('ASCII','ignore')
-
-def lmexample():
-  print "        Type 'see list_name list' to see what's in the list"
-  print "        Type 'add item to list_name list' to add an item in a list"
-  print "          In this case, if the list not exists, it will be created"
-  print "          Example: 'add cookies to shopping list'"
-  print "        Type 'remove item from list_name list'"
-  print "          Example: 'remove wash car from task list'"
 
 def example():
   print "huepy: Here are some example queries:"
   print "        Type 'lists' to see what lists you have"
   print "        Type 'bye' or 'leave' if you want me to stop bothering you"
   print "        Type 'weather' or 'forecast' to see the wheather forecast"
-  lmexample()
-  print "        WARNING: the commands that open browsers have bugs"
+  print "        Type 'see list_name list' to see what's in the list"
+  print "        Type 'add item to list_name list' to add an item in a list"
+  print "          In this case, if the list not exists, it will be created"
+  print "          Have in mind that the list's name has to be composed only by one word"
+  print "          Example: 'add cookies to shopping list'"
+  print "        Type 'remove item from list_name list'"
+  print "          Example: 'remove wash car from task list'"
   print "        You can access facebook by typing 'facebook' or 'face', and 9gag typing '9gag'"
   print "        You can also google something like the following example: 'google how does a microwave work'"
 
@@ -40,11 +37,11 @@ def llists():
 
 def checkdata(data):
   words = data.split(' ')
-  firstwords = ['see', 'put', 'add', 'remove', 'google', 'wikipedia', 'desciclopedia', 'search', 'wiki', 'desciclo', 'des', 'face']
+  firstwords = ['see', 'put', 'add', 'remove', 'buy', 'google', 'wikipedia', 'desciclopedia', 'search', 'wiki', 'desciclo', 'des', 'face']
   for f in firstwords:
     if words[0] == f:
       return f
-  keywords  = ['example', 'help', 'leave', 'bye', 'hue', 'version', 'lists', 'forecast', 'weather', 'facebook', '9gag', 'usq', 'zoer', 'zoeir', 'zuer', 'zueir', 'start']
+  keywords  = ['example', 'help', 'leave', 'bye', 'hue', 'version', 'lists', 'forecast', 'weather', 'facebook', '9gag', 'usq', 'never ends', 'zoer', 'zoeir', 'zuer', 'zueir', 'start']
   for k in keywords:
     if k in data:
       return k
@@ -81,6 +78,12 @@ def add(string):
     print 'huepy: You need to follow the format: add item_name to list_name list.'
     print '       Example: add milk to shopping list'
 
+def buy(string):
+  words = string.split(' ')
+  item = ''
+  for i in range(1, len(words)): item += words[i]+' '
+  add('add %s to shopping list' % item)
+
 def remove(string):
   words = string.split(' ')
   if len(words) > 3 and words[len(words)-1] == 'list':
@@ -89,16 +92,17 @@ def remove(string):
     for i in range(1, len(words)-3): item += words[i]+' '
     if not os.path.exists(list_name):
       print 'huepy: There is no %s list.' % words[len(words)-2]
-    with open(list_name, 'r') as arq:
-      lines = arq.readlines()
-    with open(list_name, 'w') as arq:
-      arq.close()
-    with open(list_name, 'a') as arq:
-      for line in lines:
-	if not line.strip().startswith(item.strip()):
-	  arq.write(line)
-    print 'huepy: %s removed from your %s list' % (item, words[len(words)-2])
-    see('see %s' % words[len(words)-2])
+    else:
+      with open(list_name, 'r') as arq:
+	lines = arq.readlines()
+      with open(list_name, 'w') as arq:
+	arq.close()
+      with open(list_name, 'a') as arq:
+	for line in lines:
+	  if not line.strip().startswith(item.strip()):
+	    arq.write(line)
+      print 'huepy: %s removed from your %s list' % (item, words[len(words)-2])
+      see('see %s' % words[len(words)-2])
   else:
     print 'huepy: You need to follow the format: remove item_name from list_name list.'
     print '       Example: remove milk from shopping list'
@@ -125,7 +129,7 @@ def weather():
       print '       Tonight:     %s' % ton
     except:
       errors += 1
-    if errors == 3: print '       My apologies, but I could not get none of the weather informations.'
+    if errors == 3: print '       My apologies, but I could not get anyone of the weather informations.'
   except:
     print '       It was not possible get the weather data. Please verify your conection.'
 
@@ -164,7 +168,20 @@ def face(): wb.open('https://www.facebook.com', new=2)
 def gag(): wb.open('https://www.9gag.com', new=2)
 def usq(): wb.open('https://www.umsabadoqualquer.com', new=2)
 
-def hue(): print 10 * (6 * 'HUE' + ' ' + 3 * 'BR' + '\n'), 'gibe money plox'
+def hue():
+  print 'BRBRBR      BRBRBR      BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBR      BRBRBR      BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBR      BRBRBR      BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBR      BRBRBR      BRBRBR      BRBRBR      BRBRBR            '
+  print 'BRBRBR      BRBRBR      BRBRBR      BRBRBR      BRBRBR            '
+  print 'BRBRBRBRBRBRBRBRBR      BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBRBRBRBRBRBRBR      BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBRBRBRBRBRBRBR      BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBR      BRBRBR      BRBRBR      BRBRBR      BRBRBR            '
+  print 'BRBRBR      BRBRBR      BRBRBR      BRBRBR      BRBRBR            '
+  print 'BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR      BRBRBRBRBRBRBRBRBR'
+  print 'BRBRBR      BRBRBR      BRBRBRBRBRBRBRBRBR      BRBRBRBRBRBRBRBRBR'
 def zne(): print 'huepy: The zuera never ends.'
 
 def start():
